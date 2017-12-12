e536739df9c875e8dce1533dd30969302fcd7f4f
Joe Hildebrand <joe-github@cursive.net>
2017-12-11 23:43:42 -0600

    day 12

---
+++ day12.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const fs = require('fs')
+const f = fs.readFileSync('day12.txt', {encoding: 'utf8'})
+
+const lines = f.slice(0, -1).split('\n')
+
+const routes = {}
+for (const line of lines) {
+  const m = line.match(/^(\d+)\s*<->\s*([0-9, ]+)/)
+  if (!m) continue
+  let r = routes[m[1]] || (routes[m[1]] = [])
+  r.push(...(m[2].split(/\s*,\s*/))) // careful not to create new array
+}
+
+const VISITED = Symbol('VISITED')
+
+function getLen (start) {
+  const stack = [start]
+  let c = 0
+  while (stack.length > 0) {
+    const p = stack.shift()
+    if (routes[p][VISITED]) continue
+    routes[p][VISITED] = true
+    c++
+    stack.push(...routes[p])
+  }
+  return c
+}
+
+console.log(getLen('0'))
+let groups = 1
+for (const k of Object.keys(routes)) {
+  if (routes[k][VISITED]) continue
+  groups++
+  getLen(k)
+}
+console.log(groups)
