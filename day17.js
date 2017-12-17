9c75bdcdc1f377fbc5ebd105774b6e0d957808b6
Joe Hildebrand <joe-github@cursive.net>
2017-12-17 13:50:17 -0700

    cleanup

--- day17.js
+++ day17.js
@@ -8,14 +8,16 @@ let pos = 0
 
 for (let i = 1; i <= 2017; i++) {
   pos = (pos + steps + 1) % buf.length
-  buf.splice(pos + 1, 0, i)
+  buf.splice(pos + 1, 0, i) // splice is off-by-one in this coordinate system
 }
-console.log(buf[pos+2])
+console.log(buf[pos + 2])
 
-pos = 0
 let lastz = -1
-for (let i = 1; i <= 50000000; i++) {
+for (let i = 2018; i <= 50000000; i++) {
   pos = (pos + steps + 1) % i
+  // NOTE: don't bother to save the results anymore!
+  // we only have to keep track of the last thing that
+  // was inserted after the 0.
   if (!pos) {
     lastz = i
   }
